731f6b8230f2fbb1d6b8b4c91114dfa570d16f0a
Wakholi Collins <49397327+collinswakholi@users.noreply.github.com>
2023-07-18 12:15:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open("requirements.txt", "r") as f:
     req = f.readlines()
     
 setup(
-    name='ImgTiler',
-    version='0.1.1',
+    name='img-tiler',
+    version='0.1.0',
     description='ImgTiler is a Python library for tiling and retiling images. It can be used for splitting large images into smaller tiles for other applications like Training Deep Learning models, inference on large images, etc.',
     long_description=long_descr,
     long_description_content_type='text/markdown',
