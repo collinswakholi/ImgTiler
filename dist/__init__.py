d33bb6f946dfafb87ca973dfade1e917d4a7ef8f
Wakholi Collins <49397327+collinswakholi@users.noreply.github.com>
2023-07-18 11:09:03 -0700

    Add files via upload

---
+++ dist/__init__.py
@@ -0,0 +1,17 @@
+import sys
+from ImageTiler import SplitImage, CombineTiles
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    VERSION = get_distribution(__name__).version
+except DistributionNotFound: # pragma: no cover
+    VERSION = 'unknown'
+    
+__version__ = VERSION
+__all__ = ['SplitImage', 'CombineTiles']
+
+# Import README.md as a docstring, only when generating documentation
+if "pdoc" in sys.modules:
+    with open("README.md", "r") as fh:
+        _readme = fh.read()
